23116f7fa4c08a8bf79ef1e5729f4565544023a5
benjaminyee <benhyee@gmail.com>
2018-11-26 17:35:07 -0800

    working fast set menu

--- BM92A_Funcs.c
+++ BM92A_Funcs.c
@@ -42,7 +42,6 @@ void BM92Asnk_regSet(){
     reverseDisable();chgDisable(); batt_chg = 0;
     write_word(0x1A,BM92A_ADDRESS,readTwoByte(0x1A,BM92A_ADDRESS)&0xFFFE);
     write_word(0x17,BM92A_ADDRESS,0x0000);  //config 2
-    write_word(0x26,BM92A_ADDRESS,0x8749); //system Config 1
     write_word(0x27,BM92A_ADDRESS,0x0046);  // SysConfig2
     write_word(0x2F,BM92A_ADDRESS,0xA400);  // SysConfig3
     write_word(0x06,BM92A_ADDRESS,0xCCE0);  //Controller Config 1
@@ -123,28 +122,24 @@ void BM92A_sink_PDO() {
             write_word(0x06,BM92A_ADDRESS,controllerConfig);
             PDO[0] = 0x2C; PDO[1] = 0x91;   //batteryNgt 20V max 5V min @ 3A
             PDO[2] = 0x01; PDO[3] = 0x19;
-            write_block(0x20,BM92A_ADDRESS,4,PDO);
             break;
         case 2:
             controllerConfig |= 0x0020;
             write_word(0x06,BM92A_ADDRESS,controllerConfig);
             PDO[0] = 0xc8; PDO[1] = 0x90;   //batteryNgt 15V max 5V min @ 3A
             PDO[2] = 0xc1; PDO[3] = 0x12;
-            write_block(0x20,BM92A_ADDRESS,4,PDO);
             break;
         case 3:
             controllerConfig &= 0xFFDF;
             write_word(0x06,BM92A_ADDRESS,controllerConfig);
             PDO[0] = 0x2C; PDO[1] = 0x91;   //batteryNgt 5V max 5V min @ 3A
             PDO[2] = 0x41; PDO[3] = 0x06;
-            write_block(0x20,BM92A_ADDRESS,4,PDO);
             break;
         case 4:
             controllerConfig &= 0xFFDF;
             write_word(0x06,BM92A_ADDRESS,controllerConfig);
             PDO[0] = 0x2C; PDO[1] = 0x91;   //batteryNgt 9V max 5V min @ 3A
             PDO[2] = 0x41; PDO[3] = 0x0b;
-            write_block(0x20,BM92A_ADDRESS,4,PDO);
             break;
 
         case 5:
@@ -152,18 +147,18 @@ void BM92A_sink_PDO() {
             write_word(0x06,BM92A_ADDRESS,controllerConfig);
             PDO[0] = 0xc8; PDO[1] = 0x90;   //batteryNgt 15V max 5V min @ 3A
             PDO[2] = 0xc1; PDO[3] = 0x12;
-            write_block(0x20,BM92A_ADDRESS,4,PDO);
             break;
         case 6:
             controllerConfig &= 0xFFDF;
             write_word(0x06,BM92A_ADDRESS,controllerConfig);
             PDO[0] = 0x2C; PDO[1] = 0x91;   //batteryNgt 20V max 5V min @ 3A
             PDO[2] = 0x01; PDO[3] = 0x19;
-            write_block(0x20,BM92A_ADDRESS,4,PDO);
             break;
         default:
             break;
     }
+    write_block(0x20,BM92A_ADDRESS,4,PDO);
+
     free(PDO);
 }
 void pdo100WMode(){
